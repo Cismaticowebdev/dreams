7a87f854b6077eb4df69fa3e2b10e7e713d9fd4d
Cismaticowebdev <cismaticowebdev@gmail.com>
2024-09-20 15:16:30 +0100

    Add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+    console.log(this.loginForm.status);
+  }
 }
